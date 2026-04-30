5b22354133a09928b4ecfb46a9cd4d758fc08848
ThanusriBandela <thanusribandela05@gmail.com>
2026-04-30 17:24:39 +0530

    Fix missing EnableScheduling import

--- src/main/java/com/lifelane/lifelane_ai/LifelaneAiApplication.java
+++ src/main/java/com/lifelane/lifelane_ai/LifelaneAiApplication.java
@@ -1,5 +1,6 @@
-package com.lifelane.lifelane_ai;
 
+package com.lifelane.lifelane_ai;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
